48bea574e9dfd429b022f6a1e914b149d9bc07be
zzycreate <zzycreate@gmail.com>
2019-12-21 00:36:44 +0800

    :art: LambdaHelper 新增集合分组方法

--- zf-utils/src/main/java/com/zzycreate/zzz/utils/LambdaHelper.java
+++ zf-utils/src/main/java/com/zzycreate/zzz/utils/LambdaHelper.java
@@ -1,9 +1,14 @@
 package com.zzycreate.zzz.utils;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * java8 lambda 工具
@@ -24,6 +29,59 @@ public class LambdaHelper {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>(8);
         // putIfAbsent 如果map中有值，则返回原值，新值也不会放入map中，如果原来没有值，则返回null，本次put的值也会放入map中
         return object -> seen.putIfAbsent(keyExtractor.apply(object), Boolean.TRUE) == null;
+    }
+
+    /**
+     * 将 Collection<E> 转换为 Map<K, List<V>> 结构
+     *
+     * @param collection 原始数据集合
+     * @param keyFunc    key的转换器
+     * @param valueFunc  value的转换器
+     * @param <E>        转换的集合中的元素类型
+     * @param <K>        转换后的Map的key类型
+     * @param <V>        转换后的List中的元素类型
+     * @return Map
+     */
+    public static <E, K, V> Map<K, List<V>> groupMapList(Collection<E> collection,
+                                                         Function<E, K> keyFunc, Function<E, V> valueFunc) {
+        return group(collection, keyFunc, valueFunc, Collectors.toList());
+    }
+
+    /**
+     * 将 Collection<E> 转换为 Map<K, Set<V>> 结构
+     *
+     * @param collection 原始数据集合
+     * @param keyFunc    key的转换器
+     * @param valueFunc  value的转换器
+     * @param <E>        转换的集合中的元素类型
+     * @param <K>        转换后的Map的key类型
+     * @param <V>        转换后的Set中的元素类型
+     * @return Map
+     */
+    public static <E, K, V> Map<K, Set<V>> groupMapSet(Collection<E> collection,
+                                                       Function<E, K> keyFunc, Function<E, V> valueFunc) {
+        return group(collection, keyFunc, valueFunc, Collectors.toSet());
+    }
+
+    /**
+     * 分组集合
+     *
+     * @param collection 原始数据集合
+     * @param keyFunc    key的转换器
+     * @param valueFunc  value的转换器
+     * @param collector  value的集合类型转换器
+     * @param <E>        转换的集合中的元素类型
+     * @param <K>        转换后的Map的key类型
+     * @param <V>        转换后的集合中的元素类型
+     * @param <C>        转换后的集合元素类型
+     * @return Map
+     */
+    public static <E, K, V, C> Map<K, C> group(Collection<E> collection,
+                                               Function<E, K> keyFunc, Function<E, V> valueFunc,
+                                               Collector<V, ?, C> collector) {
+        return collection.stream().collect(Collectors.groupingBy(
+                keyFunc, Collectors.mapping(valueFunc, collector)
+        ));
     }
 
 }
--- zf-utils/src/test/java/com/zzycreate/zzz/utils/LambdaHelperTest.java
+++ zf-utils/src/test/java/com/zzycreate/zzz/utils/LambdaHelperTest.java
@@ -1,18 +1,24 @@
 package com.zzycreate.zzz.utils;
 
 import com.zzycreate.zzz.utils.model.TestBean;
-import org.junit.Assert;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author zzycreate
  * @date 2019/9/26
  */
+@Slf4j
 public class LambdaHelperTest {
 
     @Test
@@ -21,24 +27,51 @@ public class LambdaHelperTest {
         List<TestBean> distinctData =
                 listData.stream().filter(LambdaHelper.distinctByKey(TestBean::getStr)).collect(Collectors.toList());
         Set<String> strSet = distinctData.stream().map(TestBean::getStr).collect(Collectors.toSet());
-        Assert.assertEquals(5, strSet.size());
+        assertEquals(5, strSet.size());
         for (int i = 0; i < 4; i++) {
             String testStr = "" + i;
-            Assert.assertTrue(strSet.contains(testStr));
+            assertTrue(strSet.contains(testStr));
         }
+    }
+
+    @Test
+    public void groupMapList() {
+        List<TestBean> listData = listData();
+        Map<String, List<Integer>> listMap = LambdaHelper.groupMapList(listData, TestBean::getStr, TestBean::getI);
+        log.info("{}", listMap);
+        assertEquals(5, listMap.size());
+        assertEquals(1, listMap.get("0").size());
+        assertEquals(2, listMap.get("1").size());
+        assertEquals(3, listMap.get("2").size());
+        assertArrayEquals(new Integer[]{2, 2, 2}, listMap.get("2").toArray());
+        assertEquals(1, listMap.get("3").size());
+        assertEquals(1, listMap.get("4").size());
+    }
+
+    @Test
+    public void groupMapSet() {
+        List<TestBean> listData = listData();
+        Map<String, Set<Integer>> mapSet = LambdaHelper.groupMapSet(listData, TestBean::getStr, TestBean::getI);
+        log.info("{}", mapSet);
+        assertEquals(5, mapSet.size());
+        assertEquals(1, mapSet.get("0").size());
+        assertEquals(1, mapSet.get("1").size());
+        assertEquals(1, mapSet.get("2").size());
+        assertArrayEquals(new Integer[]{2}, mapSet.get("2").toArray());
+        assertEquals(1, mapSet.get("3").size());
+        assertEquals(1, mapSet.get("4").size());
     }
 
     private static List<TestBean> listData() {
         List<TestBean> list = new ArrayList<>();
-        list.add(TestBean.builder().str("0").build());
-        list.add(TestBean.builder().str("1").build());
-        list.add(TestBean.builder().str("1").build());
-        list.add(TestBean.builder().str("2").build());
-        list.add(TestBean.builder().str("2").build());
-        list.add(TestBean.builder().str("3").build());
-        list.add(TestBean.builder().str("4").build());
-        list.add(TestBean.builder().str("2").build());
+        list.add(TestBean.builder().str("0").i(0).build());
+        list.add(TestBean.builder().str("1").i(1).build());
+        list.add(TestBean.builder().str("1").i(1).build());
+        list.add(TestBean.builder().str("2").i(2).build());
+        list.add(TestBean.builder().str("2").i(2).build());
+        list.add(TestBean.builder().str("3").i(3).build());
+        list.add(TestBean.builder().str("4").i(4).build());
+        list.add(TestBean.builder().str("2").i(2).build());
         return list;
     }
-
 }
